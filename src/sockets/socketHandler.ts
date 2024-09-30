28c611f3c1047485543e8b493a45186352e369b6
gabrielbotandev <gabrielficciobotan@gmail.com>
2024-09-30 19:39:48 -0300

    feat: add user connection and disconnection notifications

--- src/sockets/socketHandler.ts
+++ src/sockets/socketHandler.ts
@@ -4,6 +4,8 @@ export const setupSocket = (io: Server) => {
   io.on("connection", (socket: Socket) => {
     console.log("user connected =>", socket.id);
 
+    socket.broadcast.emit("user connected", "A user has joined the chat");
+
     socket.on("message", (msg) => {
       console.log("msg:", msg);
       io.emit("message", msg);
@@ -11,6 +13,7 @@ export const setupSocket = (io: Server) => {
 
     socket.on("disconnect", () => {
       console.log("user disconnected =>", socket.id);
+      io.emit("user disconnected", "A user has left the chat");
     });
   });
 };
