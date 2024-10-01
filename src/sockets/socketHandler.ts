7e75f40138713d2fb69e64f32e11c3088d6c65e0
gabrielbotandev <gabrielficciobotan@gmail.com>
2024-09-30 21:57:09 -0300

    feat: implement user selection for chat username and display in messages

--- src/sockets/socketHandler.ts
+++ src/sockets/socketHandler.ts
@@ -2,18 +2,20 @@ import { Server, Socket } from "socket.io";
 
 export const setupSocket = (io: Server) => {
   io.on("connection", (socket: Socket) => {
-    console.log("user connected =>", socket.id);
+    let username = "Anonymous";
 
-    socket.broadcast.emit("user connected", "A user has joined the chat");
+    socket.on("set username", (name) => {
+      username = name || "Anonymous";
+      socket.broadcast.emit("user connected", `${username} has joined the chat`);
+    });
 
-    socket.on("message", (msg) => {
-      console.log("msg:", msg);
-      io.emit("message", msg);
+    socket.on("message", (data) => {
+      console.log("Received message:", data);
+      io.emit("message", { username, msg: data.msg });
     });
 
     socket.on("disconnect", () => {
-      console.log("user disconnected =>", socket.id);
-      io.emit("user disconnected", "A user has left the chat");
+      io.emit("user disconnected", `${username} has left the chat`);
     });
   });
 };
